d07c418906cffba35eaf9714c458f110e84f512e
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-08-30 11:35:34 -0300

    atualização currículo

--- src/components/section1/Section1.jsx
+++ src/components/section1/Section1.jsx
@@ -39,7 +39,7 @@ function Section1() {
           </p>
      <div className="btn">
   <a
-    href="/Portfolio-Front-end-Developer/curriculo_frontend.pdf"
+    href="/Portfolio-Front-end-Developer/curriculo_frontend_developer.pdf"
     download="Gabriel_Rodrigues_Curriculo.pdf"
   >
     <button>Currículo</button>
