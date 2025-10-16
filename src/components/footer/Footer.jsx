dc1b45c7104c6d46ac283cc338359a18b4d93264
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-10-16 09:42:38 -0300

    correção de links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import { StylesFooter } from './Styles'
 function Footer() {
   return (
    <StylesFooter>
-        <p>&copy; Site criado por <a href="">Gabriel da Costa Rodrigues</a></p>
+        <p>&copy; Site criado por <a href="https://github.com/GRodrigues10" target='_blank'>Gabriel da Costa Rodrigues</a></p>
    </StylesFooter>
   )
 }
