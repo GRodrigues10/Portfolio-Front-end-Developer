ae7db099dee616287e55399fa0a8ae3cfa99dd66
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-11-19 14:23:57 -0300

    corrigindo emailjs

--- src/components/section5/Section5.jsx
+++ src/components/section5/Section5.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import { StylesSection5 } from "./Styles";
 import emailjs from "@emailjs/browser";
 
-
 function Section5() {
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
       .sendForm(
-        "service_w7iacft",   // Substitua
-        "template_ub7g5w2",  // Substitua
+        "service_256rbgi",     // SERVICE ID CORRETO
+        "template_c657ein",    // TEMPLATE ID CORRETO
         e.target,
-        "law81UOVlzmXzpuq-"    // Substitua
+        "YUwRXf0TSw3I7_wyr"    // PUBLIC KEY CORRETO
       )
       .then(
         () => {
@@ -25,31 +24,23 @@ function Section5() {
 
     e.target.reset();
   };
+
   return (
     <StylesSection5 id="contato">
       <div className="content-section">
         <h1>Contato</h1>
-        {/* <div className="direct-contact">
-          <div>
-            <p>
-              <strong>Telefone:</strong> (21) 96547-6339
-            </p>
-            <p>
-              <strong>Instagram:</strong> @GRodriguess10
-            </p>
-          </div>
 
-        </div> */}
         <form className="inputs" onSubmit={sendEmail}>
           <div className="names">
-            <input type="text" name="nome" placeholder="Nome" required/>
-            <input type="text" name="sobrenome" placeholder="Sobrenome" required/>
+            <input type="text" name="nome" placeholder="Nome" required />
+            <input type="text" name="sobrenome" placeholder="Sobrenome" required />
           </div>
 
           <div className="emails">
-            <input type="text" name="email" placeholder="E-mail" required/>
+            <input type="text" name="email" placeholder="E-mail" required />
             <input type="text" name="assunto" placeholder="Assunto" required />
           </div>
+
           <textarea name="mensagem" placeholder="Digite o Assunto..." required></textarea>
 
           <button type="submit">Enviar Mensagem</button>
